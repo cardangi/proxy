bad18280f45b879524300a49407106358a3b16f8
Kristian Drucker <oxkipo@googlemail.com>
2017-01-10 15:37:15 +0100

    test

--- proxy.py
+++ proxy.py
@@ -3,11 +3,11 @@ import json
 
 
 def start(container_id):
-    return 'Started container with id: ' + container_id
+    print('Started container with id: ' + container_id)
 
 
 def destroy(container_id):
-    return 'Destroyed container with id: ' + container_id
+    print('Destroyed container with id: ' + container_id)
 
 
 client = docker.Client(base_url='unix://var/run/docker.sock')
