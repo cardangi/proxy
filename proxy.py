9c0ef2d5b3ef5ce618c842769ced1a3390876520
Kristian Drucker <oxkipo@googlemail.com>
2017-01-10 15:55:57 +0100

    test

--- proxy.py
+++ proxy.py
@@ -6,7 +6,6 @@ client = docker.Client(base_url='unix://var/run/docker.sock')
 
 def start(container_id):
     container = client.inspect_container(container_id)
-    container = json.loads(container)
     print(container)
     print('Started container with id: ' + container_id)
     print('Container ' + container['Config']['Hostname'] + ', is on ip ' + container['NetworkSettings']['Networks']['bridge']['IPAddress'])
