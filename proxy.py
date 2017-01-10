a91ce9262b28b7e6e210e6dbda51dfe1586cf8ff
Kristian Drucker <oxkipo@googlemail.com>
2017-01-10 17:49:42 +0100

    test

--- proxy.py
+++ proxy.py
@@ -53,7 +53,7 @@ def sort_env(env):
 
 
 def add_host_to_json(container_id, ports, config, json_data):
-    json_data = json_data.append(('ports', ports), ('config', config))
+    json_data = json_data.append([('ports', ports), ('config', config)])
     json_data = json.dumps(json_data)
     open('data.json').write(json_data)
 
