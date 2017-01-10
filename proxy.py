0a098131394dad3e0bebabbd189f5b9a061beac0
Kristian Drucker <oxkipo@googlemail.com>
2017-01-10 15:18:41 +0100

    test

--- proxy.py
+++ proxy.py
@@ -2,4 +2,4 @@ import docker
 
 client = docker.Client(base_url='unix://var/run/docker.sock')
 for event in client.events():
-    print('Status:' + event.status + ', on container with ID: ' + event.id)
+    print('Status:' + event['status'] + ', on container with ID: ' + event['id'])
