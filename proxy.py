7af2746ce8d2a9beb65d0be2beb6e605910eba32
Kristian Drucker <oxkipo@googlemail.com>
2017-01-10 15:36:26 +0100

    test

--- proxy.py
+++ proxy.py
@@ -11,10 +11,13 @@ def destroy(container_id):
 
 
 client = docker.Client(base_url='unix://var/run/docker.sock')
+print('Started proxy')
 for event in client.events():
     event = json.loads(event)
     if 'Action' in event.keys() and 'id' in event.keys():
         if 'start' in event['Action']:
+            print('Got a start action')
             start(event['id'])
         if 'destroy' in event['Action']:
+            print('Got a destroy action')
             destroy(event['id'])
