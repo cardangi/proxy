a5bb1899252c1ef1be7c17255e041f58611a70ae
Kristian Drucker <oxkipo@googlemail.com>
2017-01-10 15:09:24 +0100

    test

--- proxy.py
+++ proxy.py
@@ -1,9 +1,5 @@
 import docker
 
-
-def main():
-    client = docker.Client(base_url='unix://var/run/docker.sock')
-    for event in client.events():
-        print(event)
-        
-main()
+client = docker.Client(base_url='unix://var/run/docker.sock')
+for event in client.events():
+    print(event)
