8f1870e8dedf88e3d6210217f6aae9030f1810ae
Kristian Drucker <oxkipo@googlemail.com>
2017-01-10 17:08:09 +0100

    test

--- proxy.py
+++ proxy.py
@@ -11,7 +11,7 @@ def start(container_id):
     ports = []
 
     for port in exp_ports:
-        print(exp_ports[port]['HostPort'])
+        print(json.dumps(exp_ports[port]))
         # ports[:0] = [exp_ports[port][0]['HostPort']]
 
     print(json.dumps(container))
