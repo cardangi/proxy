89fe6fc4dfaa7d703881ef747477bff047dd969c
Kristian Drucker <oxkipo@googlemail.com>
2017-01-11 17:22:53 +0100

    test

--- proxy.py
+++ proxy.py
@@ -34,7 +34,10 @@ def group_containers_by_env(container_id, container_ports, ip_address):
         env = sort_env(env)
         if 'VIRTUAL_HOST' in env.keys():
             if env['VIRTUAL_HOST'] not in hosts.keys():
-                hosts[env['VIRTUAL_HOST']] = {}
+                hosts[env['VIRTUAL_HOST']] = {
+                    'ip': [],
+                    'https': 'True'
+                }
             hosts[str(env['VIRTUAL_HOST'])]['ip'][:0] = ip_address + ':' + container_ports[0]
             hosts[str(env['VIRTUAL_HOST'])]['https'] = env['HTTPS']
 
